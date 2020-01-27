b0cec90c6c09363b0d00682a773fe8da33bd414c
jmknopf1007 <jmknopf1007@gmail.com>
2020-01-27 12:47:32 -0800

    Done.

---
+++ src/ColorBox.1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+export default class ColorBox extends Component {
+  render() {
+    if (this.props.opacity >= 0.2) {
+      return (<div className="color-box" style={{ opacity: this.props.opacity }}>
+        <ColorBox opacity={this.props.opacity - 0.1} />
+      </div>);
+    }
+    else {
+      return null;
+    }
+  }
+}
